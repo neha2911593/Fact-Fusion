ddc895fe903e8a73afd7938397e4228c91df8b0a
Neha <nehavasanthraj29@gmail.com>
2025-11-27 13:48:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,5 +3,6 @@ from transformers import pipeline
 # Load only ONCE (shared model)
 nli_model = pipeline(
     "text-classification",
-    model="roberta-large-mnli"
+    model_name = "facebook/bart-large-mnli"
+
 )
